fe7fc0dd145000cc12c1c939a7c3dfb7aab60db1
maciejkudej <maciejkudej765@gmail.com>
2023-06-21 14:44:50 +0200

    Add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContacts = ({ name, number }) => {
     const contact = {
       id: shortid.generate(),
